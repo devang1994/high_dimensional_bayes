e41828afd13784088cfd690bc864e6741869ff6a
Devang Agrawal <devang1994@gmail.com>
2015-11-23 01:17:59 +0000

    Restructured code, added L2 reg

--- theano_prac/mlp_blog.py
+++ theano_prac/mlp_blog.py
@@ -34,43 +34,47 @@ def model(X, w_h, w_o, b_h, b_o):
     return op
 
 
-X_train, X_test, y_train, y_test = load_blog()
 
-X = T.fmatrix(name='X')
-Y = T.fmatrix(name='Y')
+def run_test(L2reg=10,hidden_width=10,mini_batchsize=100):
 
-l1 = 10  # width of hidden layer
-input_size=X_train.shape[1]
-w_h = init_weights((input_size, l1))
-w_o = init_weights((l1, 1))
-b_h = init_bias(l1)
-b_o = init_bias(1)
+    X_train, X_test, y_train, y_test = load_blog()
 
-op = model(X, w_h, w_o, b_h, b_o)
-params = [w_h, w_o, b_h, b_o]
+    X = T.fmatrix(name='X')
+    Y = T.fmatrix(name='Y')
 
-cost = (T.mean(T.sqr(op - Y)))
-updates = sgd(cost, params)
+    input_size=X_train.shape[1]
+    w_h = init_weights((input_size, hidden_width))
+    w_o = init_weights((hidden_width, 1))
+    b_h = init_bias(hidden_width)
+    b_o = init_bias(1)
 
-train = theano.function(inputs=[X, Y], outputs=cost,
-                        updates=updates, allow_input_downcast=True,
-                        name='train')
+    op = model(X, w_h, w_o, b_h, b_o)
+    params = [w_h, w_o, b_h, b_o]
 
-predict = theano.function(inputs=[X], outputs=op, allow_input_downcast=True)
-fcost = theano.function(inputs=[op, Y], outputs=cost, allow_input_downcast=True)
-mini_batchsize = 100
-for i in range(100):
-    for start, end in zip(range(0, len(X_train), mini_batchsize), range(mini_batchsize, len(X_train), mini_batchsize)):
-        yd = (floatX(y_train[start:end])).reshape(mini_batchsize, 1)
-        cost_v = train(X_train[start:end], yd)
+    cost = (T.mean(T.sqr(op - Y)))  + T.sum(w_h ** 2) * L2reg + T.sum(w_o ** 2) * L2reg
+    updates = sgd(cost, params)
+
+    train = theano.function(inputs=[X, Y], outputs=cost,
+                            updates=updates, allow_input_downcast=True,
+                            name='train')
+    predict = theano.function(inputs=[X], outputs=op, allow_input_downcast=True)
+    fcost = theano.function(inputs=[op, Y], outputs=cost, allow_input_downcast=True)
+
+    for i in range(100):
+        for start, end in zip(range(0, len(X_train), mini_batchsize), range(mini_batchsize, len(X_train), mini_batchsize)):
+            yd = (floatX(y_train[start:end])).reshape(mini_batchsize, 1)
+            cost_v = train(X_train[start:end], yd)
+
+        fin_cost_test = fcost(predict(X_test), floatX(y_test).reshape(len(y_test), 1))
+        fin_cost_train = fcost(predict(X_train), floatX(y_train).reshape(len(y_train), 1))
+        print i, fin_cost_test, fin_cost_train
+
+    print 'final b_o values'
+    print b_o.get_value()
 
     fin_cost_test = fcost(predict(X_test), floatX(y_test).reshape(len(y_test), 1))
     fin_cost_train = fcost(predict(X_train), floatX(y_train).reshape(len(y_train), 1))
-    print i, fin_cost_test, fin_cost_train
+    print 'Hlayers: {}, BatchSize: {}, L2: {},regTrain: {}, Test: {}'.format(hidden_width,mini_batchsize,L2reg, fin_cost_train, fin_cost_test)
 
-print 'final b_o values'
-print b_o.get_value()
-
-fin_cost_test = fcost(predict(X_test), floatX(y_test).reshape(len(y_test), 1))
-fin_cost_train = fcost(predict(X_train), floatX(y_train).reshape(len(y_train), 1))
-print 'Hlayers: {}, BatchSize: {}, Train: {}, Test: {}'.format(l1,mini_batchsize, fin_cost_train, fin_cost_test)
+if __name__=="__main__":
+    run_test(L2reg=1)
