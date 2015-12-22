205139e649663fd5ea9a550728f662deca785dee
Devang Agrawal <devang1994@gmail.com>
2015-12-22 12:14:55 +0530

    finished exp4

---
+++ MLP_synthetic/logs/Exp4.md
@@ -0,0 +1,22 @@
+##Exp4
+
+Plotting evolution of test error for various number of training points. L2reg=0.01 , Hwidth = 10, MiniBatchSize=5.
+
+Reference error of 0.7297 was obtained by using simple mean prediction
+
+Atleast a 100 training points were needed for getting performance better than the reference
+
+
+![Exp1](exp4a.png)
+
+##Exp5
+
+Test error after 1000 epochs, was plotted against num training points for various amounts of L2 regularisation.
+
+
+
+###Appendix
+
+A test run of Exp4 with 10000 epochs was done.
+
+![Exp1](exp4a1.png)
--- MLP_synthetic/mlp_synthetic.py
+++ MLP_synthetic/mlp_synthetic.py
@@ -7,7 +7,10 @@ from adam import Adam
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error as MSE
 
-#this is the main code base
+epochs = 1000
+
+
+# this is the main code base
 
 def floatX(X):
     """convert to np array with floatX"""
@@ -43,13 +46,12 @@ def uniform_weights(shape):
     scale = sqrt(6. / (shape[1] + shape[0]))
     return theano.shared(floatX(np.random.uniform(low=-scale, high=scale, size=shape)))
 
-def run_test(L2reg=0.01, hidden_width=10, mini_batchsize=100,numTrainPoints=2000):
+
+def run_test(L2reg=0.01, hidden_width=10, mini_batchsize=100, numTrainPoints=2000):
     X_train, X_test, y_train, y_test = load()
 
-    X_train=X_train[:numTrainPoints]
-    y_train=y_train[:numTrainPoints]
-
-
+    X_train = X_train[:numTrainPoints]
+    y_train = y_train[:numTrainPoints]
 
     X = T.fmatrix(name='X')
     Y = T.fmatrix(name='Y')
@@ -65,78 +67,96 @@ def run_test(L2reg=0.01, hidden_width=10, mini_batchsize=100,numTrainPoints=2000
 
     cost = (T.mean(T.sqr(op - Y))) + T.sum(w_h ** 2) * L2reg + T.sum(w_o ** 2) * L2reg
     updates = sgd(cost, params)
-    #updates=Adam(cost,params)
+    # updates=Adam(cost,params)
     train = theano.function(inputs=[X, Y], outputs=cost,
                             updates=updates, allow_input_downcast=True,
                             name='train')
     predict = theano.function(inputs=[X], outputs=op, allow_input_downcast=True)
     fcost = theano.function(inputs=[op, Y], outputs=cost, allow_input_downcast=True)
 
-    test_costs=[]
-    train_costs=[]
-    epochs=1000
+    test_costs = []
+    train_costs = []
+
     for i in range(epochs):
         for start, end in zip(range(0, len(X_train), mini_batchsize),
                               range(mini_batchsize, len(X_train), mini_batchsize)):
             yd = (floatX(y_train[start:end])).reshape(mini_batchsize, 1)
             cost_v = train(X_train[start:end], yd)
 
-        #Done this cost prediction needs to change
-        #fin_cost_test = fcost(predict(X_test), floatX(y_test).reshape(len(y_test), 1))
-        #fin_cost_train = fcost(predict(X_train), floatX(y_train).reshape(len(y_train), 1))
-        fin_cost_test =MSE(predict(X_test),y_test)
-        fin_cost_train = MSE(predict(X_train),y_train)
+        # Done this cost prediction needs to change
+        # fin_cost_test = fcost(predict(X_test), floatX(y_test).reshape(len(y_test), 1))
+        # fin_cost_train = fcost(predict(X_train), floatX(y_train).reshape(len(y_train), 1))
+        fin_cost_test = MSE(predict(X_test), y_test)
+        fin_cost_train = MSE(predict(X_train), y_train)
         test_costs.append(fin_cost_test)
         train_costs.append(fin_cost_train)
-        #print i, fin_cost_test, fin_cost_train
+        # print i, fin_cost_test, fin_cost_train
 
     # print 'final b_o values'
     # print b_o.get_value()
 
-    #fin_cost_test = fcost(predict(X_test), floatX(y_test).reshape(len(y_test), 1))
-    #fin_cost_train = fcost(predict(X_train), floatX(y_train).reshape(len(y_train), 1))
-    fin_cost_test=MSE(predict(X_test),y_test)
-    fin_cost_train=MSE(predict(X_train),y_train)
+    # fin_cost_test = fcost(predict(X_test), floatX(y_test).reshape(len(y_test), 1))
+    # fin_cost_train = fcost(predict(X_train), floatX(y_train).reshape(len(y_train), 1))
+    fin_cost_test = MSE(predict(X_test), y_test)
+    fin_cost_train = MSE(predict(X_train), y_train)
     print 'Hwidth: {}, BatchSize: {}, L2reg: {},Train: {}, Test: {}'.format(hidden_width, mini_batchsize, L2reg,
                                                                             fin_cost_train, fin_cost_test)
 
 
-    #Calculate RMS error with simple mean prediction
-    test_mean=np.mean(y_test)
-    train_mean=np.mean(y_train)
+    # Calculate RMS error with simple mean prediction
+    test_mean = np.mean(y_test)
+    train_mean = np.mean(y_train)
 
-    mean_p_test=np.ones(y_test.size)*test_mean
-    mean_p_train=np.ones(y_train.size)*train_mean
+    mean_p_test = np.ones(y_test.size) * test_mean
+    mean_p_train = np.ones(y_train.size) * train_mean
 
-    #test_cost=fcost(floatX(mean_p_test).reshape(len(y_test), 1), floatX(y_test).reshape(len(y_test), 1))
-    #train_cost=fcost(floatX(mean_p_train).reshape(len(y_train), 1), floatX(y_train).reshape(len(y_train), 1))
-    test_cost=MSE(mean_p_test,y_test)
-    train_cost=MSE(mean_p_train,y_train)
+    # test_cost=fcost(floatX(mean_p_test).reshape(len(y_test), 1), floatX(y_test).reshape(len(y_test), 1))
+    # train_cost=fcost(floatX(mean_p_train).reshape(len(y_train), 1), floatX(y_train).reshape(len(y_train), 1))
+    test_cost = MSE(mean_p_test, y_test)
+    train_cost = MSE(mean_p_train, y_train)
 
-    tArray=np.ones(epochs)*test_cost
-    #print 'MSE for mean prediction, Train:{} ,Test:{}'.format(train_cost,test_cost)
+    tArray = np.ones(epochs) * test_cost
+    # print 'MSE for mean prediction, Train:{} ,Test:{}'.format(train_cost,test_cost)
 
-    plt.plot(range(epochs),test_costs,label='Test')
-    plt.plot(range(epochs),train_costs,label='Train')
-    plt.plot(range(epochs),tArray,label='Reference')
-    plt.legend()
+    plt.plot(range(epochs), test_costs, label='DatPts:{}'.format(numTrainPoints))
+    # plt.plot(range(epochs),train_costs,label='Train')
+
     plt.xlabel('Epochs')
     plt.ylabel('Error')
-    plt.title('NumTrainPoints: {}, TrainCost:{}, TestCost: {}'.format(numTrainPoints,fin_cost_train, fin_cost_test))
-    #plt.show()
-    plt.savefig('logs/exp4_TP{}.png'.format(numTrainPoints))
-    plt.close()
-    return fin_cost_train,fin_cost_test
+    # plt.title('NumTrainPoints: {}, TrainCost:{}, TestCost: {}'.format(numTrainPoints,fin_cost_train, fin_cost_test))
+    # plt.show()
+    # plt.close()
+    return fin_cost_train, fin_cost_test
+
 
 if __name__ == "__main__":
+    fin_cost_train, fin_cost_test = run_test(L2reg=0.01, hidden_width=10, numTrainPoints=10, mini_batchsize=5)
+    fin_cost_train, fin_cost_test = run_test(L2reg=0.01, hidden_width=10, numTrainPoints=20, mini_batchsize=5)
+    fin_cost_train, fin_cost_test = run_test(L2reg=0.01, hidden_width=10, numTrainPoints=30, mini_batchsize=5)
+    fin_cost_train, fin_cost_test = run_test(L2reg=0.01, hidden_width=10, numTrainPoints=50, mini_batchsize=5)
+    fin_cost_train, fin_cost_test = run_test(L2reg=0.01, hidden_width=10, numTrainPoints=100, mini_batchsize=5)
+    fin_cost_train, fin_cost_test = run_test(L2reg=0.01, hidden_width=10, numTrainPoints=200, mini_batchsize=5)
+    fin_cost_train, fin_cost_test = run_test(L2reg=0.01, hidden_width=10, numTrainPoints=500, mini_batchsize=5)
+    fin_cost_train, fin_cost_test = run_test(L2reg=0.01, hidden_width=10, numTrainPoints=1000, mini_batchsize=5)
+    fin_cost_train, fin_cost_test = run_test(L2reg=0.01, hidden_width=10, numTrainPoints=1500, mini_batchsize=5)
+    fin_cost_train, fin_cost_test = run_test(L2reg=0.01, hidden_width=10, numTrainPoints=2000, mini_batchsize=5)
+
+    X_train, X_test, y_train, y_test = load()
+
+    test_mean = np.mean(y_test)
+    train_mean = np.mean(y_train)
+    mean_p_test = np.ones(y_test.size) * test_mean
+    mean_p_train = np.ones(y_train.size) * train_mean
 
 
+    # test_cost=fcost(floatX(mean_p_test).reshape(len(y_test), 1), floatX(y_test).reshape(len(y_test), 1))
+    # train_cost=fcost(floatX(mean_p_train).reshape(len(y_train), 1), floatX(y_train).reshape(len(y_train), 1))
+    test_cost = MSE(mean_p_test, y_test)
+    train_cost = MSE(mean_p_train, y_train)
+    print test_cost
 
-    fin_cost_train,fin_cost_test=run_test(L2reg=0.01, hidden_width=10,numTrainPoints=10,mini_batchsize=5)
-    fin_cost_train,fin_cost_test=run_test(L2reg=0.01, hidden_width=10,numTrainPoints=20,mini_batchsize=5)
-    fin_cost_train,fin_cost_test=run_test(L2reg=0.01, hidden_width=10,numTrainPoints=30,mini_batchsize=5)
-    fin_cost_train,fin_cost_test=run_test(L2reg=0.01, hidden_width=10,numTrainPoints=50,mini_batchsize=5)
-    fin_cost_train,fin_cost_test=run_test(L2reg=0.01, hidden_width=10,numTrainPoints=100,mini_batchsize=5)
-    fin_cost_train,fin_cost_test=run_test(L2reg=0.01, hidden_width=10,numTrainPoints=500,mini_batchsize=5)
-    fin_cost_train,fin_cost_test=run_test(L2reg=0.01, hidden_width=10,numTrainPoints=2000,mini_batchsize=5)
-
+    tArray = np.ones(epochs) * test_cost
+    plt.plot(range(epochs), tArray, label='Reference', color='black', linewidth=2.0)
+    plt.legend()
+    plt.savefig('logs/exp4a1.png', dpi=300)
+    # plt.show()
